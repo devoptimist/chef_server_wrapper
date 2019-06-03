b169719deadda28d564f33ca7ba52789635181ee
devoptimist <sbrown@chef.io>
2019-06-03 09:17:17 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_server_wrapper
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_server_wrapper
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
