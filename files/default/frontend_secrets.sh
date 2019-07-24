7d72cb6e056f8ffe193df24a8b671618e73c9849
devoptimist <sbrown@chef.io>
2019-07-24 23:35:29 +0200

    correcting frontend data source

--- files/default/frontend_secrets.sh
+++ files/default/frontend_secrets.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 set -eu -o pipefail
 if [[ -f /etc/opscode/private-chef-secrets.json ]]; then
-    VAR1=$(jq '.veil' /etc/opscode/private-chef-secrets.json)
+    veil_hasher_secret=$(jq -r '.veil.hasher.secret' /etc/opscode/private-chef-secrets.json)
+    veil_hasher_salt=$(jq  -r '.veil.hasher.salt' /etc/opscode/private-chef-secrets.json)
+    veil_cipher_key=$(jq -r '.veil.cipher.key' /etc/opscode/private-chef-secrets.json)
+    veil_cipher_iv=$(jq -r '.veil.cipher.iv' /etc/opscode/private-chef-secrets.json)
+    veil_credentials=$(jq -r '.veil.credentials' /etc/opscode/private-chef-secrets.json)
+    VAR1=$(cat <<EOF
+{
+  "veil_hasher_secret":"${veil_hasher_secret}",
+  "veil_hasher_salt":"${veil_hasher_salt}",
+  "veil_cipher_key":"${veil_cipher_key}",
+  "veil_cipher_iv":"${veil_cipher_iv}",
+  "veil_credentials":"${veil_credentials}"
+}
+EOF
+  )
   else
-      VAR1=$(cat <<EOF
-{"veil": ""}
+    VAR1=$(cat <<EOF
+{
+  "veil_hasher_secret":"",
+  "veil_hasher_salt":"",
+  "veil_cipher_key":"",
+  "veil_cipher_iv":"",
+  "veil_credentials":""
+}
 EOF
   )
 fi
